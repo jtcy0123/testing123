b2e7b15d349e6e230c32583ffc23a6e5cb08fa9f
jtcy0123 <jasmin0123@hotmail.com>
2017-07-24 12:15:55 +0800

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   include Clearance::User
-
-  include Clearance::User
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, :format => { :with => /([^@\s]+)@([\w]+)\.([a-zA-Z]{2,})/, :message => "Invalid email" }
+  validates :password, length: { in: 6..10 }
 end
