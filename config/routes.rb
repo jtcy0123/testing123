9df55946ca18fbb5988fffc72d7474b6390ed531
jtcy0123 <jasmin0123@hotmail.com>
2017-07-24 17:26:25 +0800

    comment only when logged in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "welcome#index"
 
   resources :contents, only: [:create, :show]
+  resources :comments, only: [:create]
 
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
@@ -14,6 +15,6 @@ Rails.application.routes.draw do
   end
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
-  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
 end
